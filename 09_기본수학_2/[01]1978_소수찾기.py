bff6bc235f7072cea516a2d48b4cd34b4876c775
DeveloperMango <gomyungjin0302@gmail.com>
2021-07-27 16:48:19 +0900

    21.07.27 add python example file

---
+++ 09_기본수학_2/[01]1978_소수찾기.py
@@ -0,0 +1,15 @@
+N = int(input())
+n = list(map(int, input().split()))
+result = 0
+
+for i in n:
+    sosu = 0
+    
+    if i>1:
+        for j in range(2, i+1):
+            if i%j == 0:
+                sosu += 1
+    if sosu==1:
+        result += 1
+
+print(result)
---
+++ 09_기본수학_2/[05]4948_베르트랑공준.py
@@ -0,0 +1,17 @@
+#소수찾기 -> 에라토스테네스의 체
+
+def prime_list(n):
+    nums = [0, 0] + [1] * (2*n-1)
+    
+    for i in range(2, int((2*n)**0.5)+1):
+        if nums[i] == 1:
+            nums[2*i::i] = [0] * ((2*n-i)//i)
+    return nums[n+1:].count(1)
+
+while True:
+    n = int(input())
+    
+    if n == 0:
+        break
+    else:
+        print(prime_list(n))
