69f1981ac900983a952c0ac70c6251d3a3d4371e
solum_kmj-git01 <myungjin.go@solu-m.com>
2021-09-10 16:57:18 +0900

    21.09.10 Studying Basic math algorithm

--- 08_기본수학_1/[06]2775번_부회장_개발중.py
+++ 08_기본수학_1/[06]2775번_부회장_개발중.py
@@ -1,3 +1,17 @@
 #2775번: 부회장이 될테야
 #https://www.acmicpc.net/problem/2775
 
+import sys
+cnt = int(sys.stdin.readline())
+
+for i in range(cnt):
+    floor = int(sys.stdin.readline())
+    room = int(sys.stdin.readline())
+    res = 0
+    
+    for i in range(floor):
+        for j in range(1,room+1):
+            if i == 0:
+                res += j
+            else:
+                
